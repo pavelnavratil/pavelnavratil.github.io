a71689a43b917d494aeed2cff2bb7a91d02ba659
Pavel Navratil <navratilpavel@users.noreply.github.com>
2025-03-11 20:27:37 +0100

    Page source moved from other repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://inp.cz',
+	output: 'server',
+  	adapter: cloudflare(),
 	integrations: [tailwind()],
+	i18n: {
+        locales: ['en', 'cs'],
+        defaultLocale: 'en',
+        routing: {
+           prefixDefaultLocale: true
+        },
+		fallback: {
+			cs: "en"
+		}
+    }
 });
