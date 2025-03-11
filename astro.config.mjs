49f9426fe3672fb19244c540061547e552d4e420
Pavel Navratil <navratilpavel@users.noreply.github.com>
2025-03-11 20:38:53 +0100

    Remove locale config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://inp.cz',
-	integrations: [tailwind()],
-	i18n: {
-        locales: ['en', 'cs'],
-        defaultLocale: 'en',
-        routing: {
-           prefixDefaultLocale: true
-        },
-		fallback: {
-			cs: "en"
-		}
-    }
+	integrations: [tailwind()]
 });
