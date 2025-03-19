7bb12c5a8b6b572af063129a88bc3a10eb91190a
Pavel Navratil <navratilpavel@users.noreply.github.com>
2025-03-19 20:58:30 +0100

    Particular fix of moon to sun change

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -2,38 +2,38 @@ type Locale = "en" | "cs" | string;
 
 type Translation = {
   [locale in Locale]: {
-    [key: string]: string
-  }
+    [key: string]: string;
+  };
 };
 
 type Languages = {
-  [locale in Locale]: string
-}
+  [locale in Locale]: string;
+};
 
 export type { Locale, Translation, Languages };
 
 export const languages: Languages = {
-  en: 'English',
-  cs: 'Čeština',
+  en: "English",
+  cs: "Čeština",
 };
 
-export const defaultLang = 'en';
+export const defaultLang = "en";
 
 export const ui: Translation = {
   en: {
-    'nav.home': 'Home',
-    'nav.what': 'What I Do',
-    'nav.who': 'Who I Am',
-    'nav.worked': 'My Work',
-    'nav.projects': 'My Projects',
-    'nav.posts': 'Blog',
+    "nav.home": "Home",
+    "nav.what": "What I Do",
+    "nav.who": "Who I Am",
+    "nav.worked": "My Work",
+    "nav.projects": "My Projects",
+    "nav.posts": "Blog",
   },
   cs: {
-    'nav.home': 'Domů',
-    'nav.what': 'Co dělám',
-    'nav.who': 'Kdo jsem',
-    'nav.worked': 'Moje práce',
-    'nav.projects': 'Moje projekty',
-    'nav.posts': 'Blog',
+    "nav.home": "Domů",
+    "nav.what": "Co dělám",
+    "nav.who": "Kdo jsem",
+    "nav.worked": "Moje práce",
+    "nav.projects": "Moje projekty",
+    "nav.posts": "Blog",
   },
 } as const;
